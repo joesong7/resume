94382719cfa08af51533ab11ca4c0ab29c42e0fa
joesong <songrongyu@songrongyudeMacBook-Air.local>
2022-02-17 15:38:34 +0800

    新增react 比例圖

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -47,6 +47,12 @@ export default {
           color:"progress-bar green"
         },
         {
+          num: 70,
+          name: "react",
+          img: "data:image/svg+xml;base64,PHN2ZyB4bWxucz0iaHR0cDovL3d3dy53My5vcmcvMjAwMC9zdmciIHZpZXdCb3g9Ii0xMS41IC0xMC4yMzE3NCAyMyAyMC40NjM0OCI+CiAgPHRpdGxlPlJlYWN0IExvZ288L3RpdGxlPgogIDxjaXJjbGUgY3g9IjAiIGN5PSIwIiByPSIyLjA1IiBmaWxsPSIjNjFkYWZiIi8+CiAgPGcgc3Ryb2tlPSIjNjFkYWZiIiBzdHJva2Utd2lkdGg9IjEiIGZpbGw9Im5vbmUiPgogICAgPGVsbGlwc2Ugcng9IjExIiByeT0iNC4yIi8+CiAgICA8ZWxsaXBzZSByeD0iMTEiIHJ5PSI0LjIiIHRyYW5zZm9ybT0icm90YXRlKDYwKSIvPgogICAgPGVsbGlwc2Ugcng9IjExIiByeT0iNC4yIiB0cmFuc2Zvcm09InJvdGF0ZSgxMjApIi8+CiAgPC9nPgo8L3N2Zz4K",
+          color: "progress-bar blue",
+        },
+        {
           num: 50,
           name: "laravel",
           img: "https://laravel.com/img/logomark.min.svg",
@@ -58,6 +64,7 @@ export default {
           img: "https://miro.medium.com/max/325/1*K8Xv0uWQ63F3G00iTbqMlQ.png",
           color: "progress-bar orange",
         },
+        
 
       ],
     });
@@ -68,6 +75,9 @@ export default {
 <style scoped>
 .orange{
   background-color: #f29c2b !important;
+}
+.blue{
+  background-color: #1F6ED4 !important;
 }
 .green{
   background-color: #8bc24c !important;
